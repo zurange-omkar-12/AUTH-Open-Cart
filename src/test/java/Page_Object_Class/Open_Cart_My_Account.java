57e065f1e33c607b6590e901d56c66156e2cf54d
zurange-omkar-12 <zurangeomkar7@gmail.com>
2024-04-12 00:38:22 +0530

    Added DataProvider

--- src/test/java/Page_Object_Class/Open_Cart_My_Account.java
+++ src/test/java/Page_Object_Class/Open_Cart_My_Account.java
@@ -15,12 +15,21 @@ public class Open_Cart_My_Account extends Base_Class_init{
 	
 	@FindBy(xpath="//h2[normalize-space()='My Account']")WebElement My_Account_Label;
 	
+	@FindBy(xpath="//span[normalize-space()='My Account']") WebElement nav_My_Account;
+	
+	@FindBy(xpath="//ul[@class='dropdown-menu dropdown-menu-right']//a[normalize-space()='Logout']")WebElement nav_Logout;
+	
 	
 	public boolean Check_My_Account_Label()
 	{
 		return My_Account_Label.isDisplayed();
 	}
 	
+	public void Click_Logout()
+	{
+		nav_My_Account.click();
+		nav_Logout.click();
+	}
 	
 	
 }
--- src/test/java/Test_Cases_Package/Test_Case_03.java
+++ src/test/java/Test_Cases_Package/Test_Case_03.java
@@ -1,6 +1,9 @@
 package Test_Cases_Package;
 
+import java.util.List;
+
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import Page_Object_Class.Open_Cart_Account_Login;
@@ -10,8 +13,8 @@ import Page_Object_Class.Open_Cart_My_Account;
 public class Test_Case_03 extends Base_Class_For_Test_cases {
 
 	
-	@Test
-	void Test_Case_Login_Page()
+	@Test(dataProvider = "Email_Password")
+	void Test_Case_Login_Page(User_Details Temp)
 	{
 		Home_Page_Obj=new Open_Cart_Home_Page(Driver_Obj);
 		
@@ -19,14 +22,19 @@ public class Test_Case_03 extends Base_Class_For_Test_cases {
 		
 		Account_Login = new Open_Cart_Account_Login(Driver_Obj);
 		
-		Account_Login.Login(Properties_Obj.getProperty("Email"),Properties_Obj.getProperty("Password"));
+		//Account_Login.Login(Properties_Obj.getProperty("Email"),Properties_Obj.getProperty("Password"));
+		
+		Account_Login.Login(Temp.get_username(),Temp.get_password());
 		
 		My_Account = new Open_Cart_My_Account(Driver_Obj);
 		
 		Assert.assertTrue(My_Account.Check_My_Account_Label());
+		My_Account.Click_Logout();
 						
 		}
 	
+
+
 	
 	
 }
